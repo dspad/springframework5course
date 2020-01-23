fb00512c09299b3f0ff207a68dc06eb10dd0330c
Daniele Spadoni <daniele.spadoni@technisblu.it>
2020-01-23 19:13:06 +0100

    Thymeleaf introduction

--- spring5webapp/src/main/java/it/technisblu/spring5webapp/bootstrap/DevBootstrap.java
+++ spring5webapp/src/main/java/it/technisblu/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,11 @@ package it.technisblu.spring5webapp.bootstrap;
 
 import it.technisblu.spring5webapp.model.Author;
 import it.technisblu.spring5webapp.model.Book;
+import it.technisblu.spring5webapp.model.Publisher;
 import it.technisblu.spring5webapp.repositories.AuthorRepository;
 import it.technisblu.spring5webapp.repositories.BookRepository;
+import it.technisblu.spring5webapp.repositories.PublisherRepository;
+
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -16,10 +19,12 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -29,23 +34,25 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private void initData(){
 
-        //Eric
-        Author eric = new Author("Eric", "Evans");
-        Book  ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
-        eric.getBooks().add(ddd);
-        ddd.getAuthors().add(eric);
+        Author spadoni = new Author("Daniele", "Spadoni");
+        Publisher publisherTech = new Publisher("Technis Blu","Viale Luigi Schiavonetti 290");
+        Book  simog = new Book("Simog per negati", "1234", publisherTech);
+        spadoni.getBooks().add(simog);
+        simog.getAuthors().add(spadoni);
 
-        authorRepository.save(eric);
-        bookRepository.save(ddd);
-
-
-        //Rod
-        Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "23444", "Wrox" );
-        rod.getBooks().add(noEJB);
-        noEJB.getAuthors().add(rod);
-
-        authorRepository.save(rod);
-        bookRepository.save(noEJB);
+        Author andreini = new Author("Daniele", "Andreini");
+        Book supporto = new Book("Come gestire un ticket", "23444", publisherTech );
+        andreini.getBooks().add(supporto);
+        supporto.getAuthors().add(andreini);
+        
+        publisherRepository.save(publisherTech);
+        saveBook(spadoni,simog);
+        saveBook(andreini,supporto);
+       
+    }
+    
+    private void saveBook(Author author, Book book) {
+        authorRepository.save(author);
+        bookRepository.save(book);
     }
 }
